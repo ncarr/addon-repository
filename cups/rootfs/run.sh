a2406954d20b350a2939c8704ce372eb07baaa01
Nicholas Carr <ncarr@users.noreply.github.com>
2020-12-31 03:28:03 -0500

    Escaped newlines

--- cups/rootfs/run.sh
+++ cups/rootfs/run.sh
@@ -2,12 +2,12 @@
 
 tempio \
     -conf /data/options.json \
-    -template /usr/share/cupsd.conf.tempio
+    -template /usr/share/cupsd.conf.tempio \
     -out /etc/cups/cupsd.conf
 
 tempio \
     -conf /data/options.json \
-    -template /usr/share/cups-files.conf.tempio
+    -template /usr/share/cups-files.conf.tempio \
     -out /etc/cups/cups-files.conf
 
 /usr/sbin/cupsd -f
