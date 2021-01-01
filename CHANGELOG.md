0463ac849c53d3eeb4e933092e3a5d858ee80651
Nicholas Carr <ncarr@users.noreply.github.com>
2021-01-01 04:45:12 -0500

    Create avahi service and generate host allowlist

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+
+## 1.0.0-alpha.3
+
+- Added this changelog
+- Added all configured hostnames to allowlist
+- Added avahi-daemon service
+
+## 1.0.0-alpha.2
+
+- Updated link to repository
+- Fixed unescaped newlines causing container start to fail in run.sh
+
+## 1.0.0-alpha.1
+
+- Initial release
--- cups/rootfs/run.sh
+++ cups/rootfs/run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/with-contenv bashio
 
-tempio \
-    -conf /data/options.json \
+result=$(bashio::api.supervisor GET /core/api/config true || true)
+internal=$(bashio::jq "${result}" '.internal_url' | cut -d'/' -f3 | cut -d':' -f1)
+external=$(bashio::jq "${result}" '.external_url' | cut -d'/' -f3 | cut -d':' -f1)
+hostname=$(bashio::info.hostname)
+
+jq --arg internal "${internal}" --arg external "${external}" --arg hostname "${hostname}" \
+    '{ssl: .ssl, require_ssl: .require_ssl, internal: $internal, external: $internal, hostname: $hostname}' \
+    /data/options.json | tempio \
     -template /usr/share/cupsd.conf.tempio \
     -out /etc/cups/cupsd.conf
 
